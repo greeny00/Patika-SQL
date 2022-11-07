e333ba941c01fe5e23b5e2f81bc3e8722b6cca06
greeny00 <93272032+greeny00@users.noreply.github.com>
2022-11-07 18:28:43 +0300

    Update HW1.sql

--- HW1.sql
+++ HW1.sql
@@ -1,5 +1,9 @@
 select title, description from film;
+
 select length from film where length between 60 AND 75;
+
 select * from film where rental_rate =0.99 AND replacement_cost=12.99 or replacement_cost=28.99;
+
 select first_name, last_name from customer where first_name='Mary';
+
 select length,rental_rate from film where length <= 50 and rental_rate!=2.99 and rental_rate!=4.99;
